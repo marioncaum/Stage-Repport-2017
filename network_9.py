efae33143ae4ffc12a615672882575878358c7d8
marioncaum <30866043+marioncaum@users.noreply.github.com>
2017-08-10 09:50:35 +0200

    Update network_9.py

--- network_9.py
+++ network_9.py
@@ -13,7 +13,6 @@ from pylearn2.training_algorithms.learning_rule import MomentumAdjustor, Momentu
 
 # Importing the format in which the model will be trained
 theano.config.floatX = sys.argv[1]
-theano.config.device = gpu
 
 # Loading the MNIST dataset
 train_set = MNIST(which_set = 'train',
